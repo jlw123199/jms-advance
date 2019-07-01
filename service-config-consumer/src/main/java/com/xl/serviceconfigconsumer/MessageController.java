b47987887e3aacf4741efd246d9766cd2137dbda
jia.liwei <jia.liwei@rd.com>
2019-07-01 18:12:18 +0800

    Using redis config with configuration service

--- service-config-consumer/src/main/java/com/xl/serviceconfigconsumer/MessageController.java
+++ service-config-consumer/src/main/java/com/xl/serviceconfigconsumer/MessageController.java
@@ -2,26 +2,41 @@ package com.xl.serviceconfigconsumer;
 
 import com.xl.serviceconfigconsumer.config.ServiceConfig;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RefreshScope
+//@RefreshScope
 @RestController
 @Service
 public class MessageController {
 
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+
     @Autowired
     private ServiceConfig serviceConfig;
 
-    @Value("${message:Hello default}")
-    private String message;
+//    @Value("${message:Hello default}")
+//    private String message;
 
 
     @RequestMapping("/message")
     String getMessage() {
-        return this.message  + serviceConfig.getExampleProperty();
+
+        String name = "保存字符串11";
+        // 保存字符串
+        stringRedisTemplate.opsForValue().set("name", name);
+        String rString =  stringRedisTemplate.opsForValue().get("name");
+
+        return  rString + serviceConfig.getExampleProperty();
+//        return this.message  + serviceConfig.getExampleProperty();
     }
+
+
 }
+
+
--- service-config-consumer/src/main/java/com/xl/serviceconfigconsumer/config/ServiceConfig.java
+++ service-config-consumer/src/main/java/com/xl/serviceconfigconsumer/config/ServiceConfig.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class ServiceConfig {
 
-    @Value("${example.property}")
+//    @Value("${example.property}")
     private String exampleProperty;
 
     public String getExampleProperty(){
