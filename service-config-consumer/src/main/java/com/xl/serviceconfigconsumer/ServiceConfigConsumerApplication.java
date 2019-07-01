a261fc8df9cf7fc6dbace8261483927abb1dd608
jia.liwei <jia.liwei@rd.com>
2019-07-01 11:45:34 +0800

    Fixed compile issue

--- service-config-consumer/src/main/java/com/xl/serviceconfigconsumer/ServiceConfigConsumerApplication.java
+++ service-config-consumer/src/main/java/com/xl/serviceconfigconsumer/ServiceConfigConsumerApplication.java
@@ -3,6 +3,8 @@ package com.xl.serviceconfigconsumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableAutoConfiguration
